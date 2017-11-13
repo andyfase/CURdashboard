0d7733e4af97111467cb595e60ba9be6d94a91e5
Andy <andrfase@a0999b11e339.ant.amazon.com>
2017-11-12 20:43:44 -0800

    Comment out RI code for now. Needs to be updated for CUR

--- go/analyzeCUR/analyzeCUR.go
+++ go/analyzeCUR/analyzeCUR.go
@@ -25,8 +25,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/athena"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/mohae/deepcopy"
 )
 
 /*
@@ -315,132 +313,132 @@ Function processes a single hours worth of RI usage and compares against availab
 */
 func riUtilizationHour(svc *cloudwatch.CloudWatch, date string, used map[string]map[string]map[string]int, azRI map[string]map[string]map[string]int, regionRI map[string]map[string]int, conf Config, region string) error {
 
-	// Perform Deep Copy of both RI maps.
-	// We need a copy of the maps as we decrement the RI's available by the hourly usage and a map is a pointer
-	// hence decrementing the original maps will affect the pass-by-reference data
-	cpy := deepcopy.Copy(azRI)
-	t_azRI, ok := cpy.(map[string]map[string]map[string]int)
-	if !ok {
-		return errors.New("could not copy AZ RI map")
-	}
+	// // Perform Deep Copy of both RI maps.
+	// // We need a copy of the maps as we decrement the RI's available by the hourly usage and a map is a pointer
+	// // hence decrementing the original maps will affect the pass-by-reference data
+	// cpy := deepcopy.Copy(azRI)
+	// t_azRI, ok := cpy.(map[string]map[string]map[string]int)
+	// if !ok {
+	// 	return errors.New("could not copy AZ RI map")
+	// }
 
-	cpy = deepcopy.Copy(regionRI)
-	t_regionRI, ok := cpy.(map[string]map[string]int)
-	if !ok {
-		return errors.New("could not copy Regional RI map")
-	}
+	// cpy = deepcopy.Copy(regionRI)
+	// t_regionRI, ok := cpy.(map[string]map[string]int)
+	// if !ok {
+	// 	return errors.New("could not copy Regional RI map")
+	// }
 
-	// Iterate through used hours decrementing any available RI's per hour's that were used
-	// AZ specific RI's are first checked and then regional RI's
-	for az := range used {
-		for instance := range used[az] {
-			// check if azRI for this region even exist
-			_, ok := t_azRI[az][instance]
-			if ok {
-				for platform := range used[az][instance] {
-					// check if azRI for this region and platform even exists
-					_, ok2 := t_azRI[az][instance][platform]
-					if ok2 {
-						// More RI's than we used
-						if t_azRI[az][instance][platform] >= used[az][instance][platform] {
-							t_azRI[az][instance][platform] -= used[az][instance][platform]
-							used[az][instance][platform] = 0
-						} else {
-							// Less RI's than we used
-							used[az][instance][platform] -= t_azRI[az][instance][platform]
-							t_azRI[az][instance][platform] = 0
-						}
-					}
-				}
-			}
+	// // Iterate through used hours decrementing any available RI's per hour's that were used
+	// // AZ specific RI's are first checked and then regional RI's
+	// for az := range used {
+	// 	for instance := range used[az] {
+	// 		// check if azRI for this region even exist
+	// 		_, ok := t_azRI[az][instance]
+	// 		if ok {
+	// 			for platform := range used[az][instance] {
+	// 				// check if azRI for this region and platform even exists
+	// 				_, ok2 := t_azRI[az][instance][platform]
+	// 				if ok2 {
+	// 					// More RI's than we used
+	// 					if t_azRI[az][instance][platform] >= used[az][instance][platform] {
+	// 						t_azRI[az][instance][platform] -= used[az][instance][platform]
+	// 						used[az][instance][platform] = 0
+	// 					} else {
+	// 						// Less RI's than we used
+	// 						used[az][instance][platform] -= t_azRI[az][instance][platform]
+	// 						t_azRI[az][instance][platform] = 0
+	// 					}
+	// 				}
+	// 			}
+	// 		}
 
-			// check if regionRI even exists and that instance used is in the right region
-			_, ok = t_regionRI[instance]
-			if ok && az[:len(az)-1] == region {
-				for platform := range used[az][instance] {
-					// if we still have more used instances check against regional RI's
-					if used[az][instance][platform] > 0 && t_regionRI[instance][platform] > 0 {
-						if t_regionRI[instance][platform] >= used[az][instance][platform] {
-							t_regionRI[instance][platform] -= used[az][instance][platform]
-							used[az][instance][platform] = 0
-						} else {
-							used[az][instance][platform] -= t_regionRI[instance][platform]
-							t_regionRI[instance][platform] = 0
-						}
-					}
-				}
-			}
-		}
-	}
+	// 		// check if regionRI even exists and that instance used is in the right region
+	// 		_, ok = t_regionRI[instance]
+	// 		if ok && az[:len(az)-1] == region {
+	// 			for platform := range used[az][instance] {
+	// 				// if we still have more used instances check against regional RI's
+	// 				if used[az][instance][platform] > 0 && t_regionRI[instance][platform] > 0 {
+	// 					if t_regionRI[instance][platform] >= used[az][instance][platform] {
+	// 						t_regionRI[instance][platform] -= used[az][instance][platform]
+	// 						used[az][instance][platform] = 0
+	// 					} else {
+	// 						used[az][instance][platform] -= t_regionRI[instance][platform]
+	// 						t_regionRI[instance][platform] = 0
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	// Now loop through the temp RI data to check if any RI's are still available
-	// If they are and the % of un-use is above the configured threshold then colate for sending to cloudwatch
-	// We sum up the total of regional and AZ specific RI's so that we get one instance based metric regardless of region or AZ RI
-	i_unused := make(map[string]map[string]int)
-	i_total := make(map[string]map[string]int)
-	var unused int
-	var total int
+	// // Now loop through the temp RI data to check if any RI's are still available
+	// // If they are and the % of un-use is above the configured threshold then colate for sending to cloudwatch
+	// // We sum up the total of regional and AZ specific RI's so that we get one instance based metric regardless of region or AZ RI
+	// i_unused := make(map[string]map[string]int)
+	// i_total := make(map[string]map[string]int)
+	// var unused int
+	// var total int
 
-	for az := range t_azRI {
-		for instance := range t_azRI[az] {
-			_, ok := i_unused[instance]
-			if !ok {
-				i_unused[instance] = make(map[string]int)
-				i_total[instance] = make(map[string]int)
-			}
-			for platform := range t_azRI[az][instance] {
-				i_total[instance][platform] = azRI[az][instance][platform]
-				i_unused[instance][platform] = t_azRI[az][instance][platform]
-				total += azRI[az][instance][platform]
-				unused += t_azRI[az][instance][platform]
-			}
-		}
-	}
+	// for az := range t_azRI {
+	// 	for instance := range t_azRI[az] {
+	// 		_, ok := i_unused[instance]
+	// 		if !ok {
+	// 			i_unused[instance] = make(map[string]int)
+	// 			i_total[instance] = make(map[string]int)
+	// 		}
+	// 		for platform := range t_azRI[az][instance] {
+	// 			i_total[instance][platform] = azRI[az][instance][platform]
+	// 			i_unused[instance][platform] = t_azRI[az][instance][platform]
+	// 			total += azRI[az][instance][platform]
+	// 			unused += t_azRI[az][instance][platform]
+	// 		}
+	// 	}
+	// }
 
-	for instance := range t_regionRI {
-		for platform := range t_regionRI[instance] {
-			_, ok := i_unused[instance]
-			if !ok {
-				i_unused[instance] = make(map[string]int)
-				i_total[instance] = make(map[string]int)
-			}
-			i_total[instance][platform] += regionRI[instance][platform]
-			i_unused[instance][platform] += t_regionRI[instance][platform]
-			total += regionRI[instance][platform]
-			unused += t_regionRI[instance][platform]
-		}
-	}
+	// for instance := range t_regionRI {
+	// 	for platform := range t_regionRI[instance] {
+	// 		_, ok := i_unused[instance]
+	// 		if !ok {
+	// 			i_unused[instance] = make(map[string]int)
+	// 			i_total[instance] = make(map[string]int)
+	// 		}
+	// 		i_total[instance][platform] += regionRI[instance][platform]
+	// 		i_unused[instance][platform] += t_regionRI[instance][platform]
+	// 		total += regionRI[instance][platform]
+	// 		unused += t_regionRI[instance][platform]
+	// 	}
+	// }
 
-	// loop over per-instance utilization and build metrics to send
-	metrics := AthenaResponse{}
-	for instance := range i_unused {
-		_, ok := conf.RI.Ignore[instance]
-		if !ok { // instance not on ignore list
-			for platform := range i_unused[instance] {
-				percent := (float64(i_unused[instance][platform]) / float64(i_total[instance][platform])) * 100
-				if int(percent) > conf.RI.PercentThreshold && i_total[instance][platform] > conf.RI.TotalThreshold {
-					metrics.Rows = append(metrics.Rows, map[string]string{"dimension": "instance=" + instance + ",platform=" + platform, "date": date, "value": strconv.FormatInt(int64(percent), 10)})
-				}
-			}
-		}
-	}
+	// // loop over per-instance utilization and build metrics to send
+	// metrics := AthenaResponse{}
+	// for instance := range i_unused {
+	// 	_, ok := conf.RI.Ignore[instance]
+	// 	if !ok { // instance not on ignore list
+	// 		for platform := range i_unused[instance] {
+	// 			percent := (float64(i_unused[instance][platform]) / float64(i_total[instance][platform])) * 100
+	// 			if int(percent) > conf.RI.PercentThreshold && i_total[instance][platform] > conf.RI.TotalThreshold {
+	// 				metrics.Rows = append(metrics.Rows, map[string]string{"dimension": "instance=" + instance + ",platform=" + platform, "date": date, "value": strconv.FormatInt(int64(percent), 10)})
+	// 			}
+	// 		}
+	// 	}
+	// }
 
-	// send per instance type under-utilization
-	if len(metrics.Rows) > 0 {
-		if err := sendMetric(svc, metrics, conf.General.Namespace, conf.RI.CwName, conf.RI.CwType, conf.RI.CwDimension); err != nil {
-			log.Fatal(err)
-		}
-	}
+	// // send per instance type under-utilization
+	// if len(metrics.Rows) > 0 {
+	// 	if err := sendMetric(svc, metrics, conf.General.Namespace, conf.RI.CwName, conf.RI.CwType, conf.RI.CwDimension); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 
-	// If confured send overall total utilization
-	if conf.RI.TotalUtilization {
-		percent := 100 - ((float64(unused) / float64(total)) * 100)
-		total := AthenaResponse{}
-		total.Rows = append(total.Rows, map[string]string{"dimension": "hourly", "date": date, "value": strconv.FormatInt(int64(percent), 10)})
-		if err := sendMetric(svc, total, conf.General.Namespace, conf.RI.CwNameTotal, conf.RI.CwType, conf.RI.CwDimensionTotal); err != nil {
-			log.Fatal(err)
-		}
-	}
+	// // If confured send overall total utilization
+	// if conf.RI.TotalUtilization {
+	// 	percent := 100 - ((float64(unused) / float64(total)) * 100)
+	// 	total := AthenaResponse{}
+	// 	total.Rows = append(total.Rows, map[string]string{"dimension": "hourly", "date": date, "value": strconv.FormatInt(int64(percent), 10)})
+	// 	if err := sendMetric(svc, total, conf.General.Namespace, conf.RI.CwNameTotal, conf.RI.CwType, conf.RI.CwDimensionTotal); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 
 	return nil
 }
@@ -451,90 +449,90 @@ Then loops through every hour and calls riUtilizationHour to process each hours 
 */
 func riUtilization(sess *session.Session, svcAthena *athena.Athena, conf Config, key string, secret string, region string, account string, date string) error {
 
-	svc := ec2.New(sess)
+	// svc := ec2.New(sess)
 
-	params := &ec2.DescribeReservedInstancesInput{
-		DryRun: aws.Bool(false),
-		Filters: []*ec2.Filter{
-			{
-				Name: aws.String("state"),
-				Values: []*string{
-					aws.String("active"),
-				},
-			},
-		},
-	}
+	// params := &ec2.DescribeReservedInstancesInput{
+	// 	DryRun: aws.Bool(false),
+	// 	Filters: []*ec2.Filter{
+	// 		{
+	// 			Name: aws.String("state"),
+	// 			Values: []*string{
+	// 				aws.String("active"),
+	// 			},
+	// 		},
+	// 	},
+	// }
 
-	resp, err := svc.DescribeReservedInstances(params)
-	if err != nil {
-		return err
-	}
+	// resp, err := svc.DescribeReservedInstances(params)
+	// if err != nil {
+	// 	return err
+	// }
 
-	az_ri := make(map[string]map[string]map[string]int)
-	region_ri := make(map[string]map[string]int)
+	// az_ri := make(map[string]map[string]map[string]int)
+	// region_ri := make(map[string]map[string]int)
 
-	// map in number of RI's available both AZ specific and regional
-	for i := range resp.ReservedInstances {
-		ri := resp.ReservedInstances[i]
+	// // map in number of RI's available both AZ specific and regional
+	// for i := range resp.ReservedInstances {
+	// 	ri := resp.ReservedInstances[i]
 
-		// Trim VPC identifier of Platform type as its not relevant for RI Utilization calculations
-		platform := strings.TrimSuffix(*ri.ProductDescription, " (Amazon VPC)")
+	// 	// Trim VPC identifier of Platform type as its not relevant for RI Utilization calculations
+	// 	platform := strings.TrimSuffix(*ri.ProductDescription, " (Amazon VPC)")
 
-		if *ri.Scope == "Availability Zone" {
-			_, ok := az_ri[*ri.AvailabilityZone]
-			if !ok {
-				az_ri[*ri.AvailabilityZone] = make(map[string]map[string]int)
-			}
-			_, ok = az_ri[*ri.AvailabilityZone][*ri.InstanceType]
-			if !ok {
-				az_ri[*ri.AvailabilityZone][*ri.InstanceType] = make(map[string]int)
-			}
-			az_ri[*ri.AvailabilityZone][*ri.InstanceType][platform] += int(*ri.InstanceCount)
-		} else if *ri.Scope == "Region" {
-			_, ok := region_ri[*ri.InstanceType]
-			if !ok {
-				region_ri[*ri.InstanceType] = make(map[string]int)
-			}
-			region_ri[*ri.InstanceType][platform] += int(*ri.InstanceCount)
-		}
-	}
+	// 	if *ri.Scope == "Availability Zone" {
+	// 		_, ok := az_ri[*ri.AvailabilityZone]
+	// 		if !ok {
+	// 			az_ri[*ri.AvailabilityZone] = make(map[string]map[string]int)
+	// 		}
+	// 		_, ok = az_ri[*ri.AvailabilityZone][*ri.InstanceType]
+	// 		if !ok {
+	// 			az_ri[*ri.AvailabilityZone][*ri.InstanceType] = make(map[string]int)
+	// 		}
+	// 		az_ri[*ri.AvailabilityZone][*ri.InstanceType][platform] += int(*ri.InstanceCount)
+	// 	} else if *ri.Scope == "Region" {
+	// 		_, ok := region_ri[*ri.InstanceType]
+	// 		if !ok {
+	// 			region_ri[*ri.InstanceType] = make(map[string]int)
+	// 		}
+	// 		region_ri[*ri.InstanceType][platform] += int(*ri.InstanceCount)
+	// 	}
+	// }
 
-	// Fetch RI hours used
-	data, err := sendQuery(svcAthena, conf.Athena.DbName, substituteParams(conf.RI.Sql, map[string]string{"**DATE**": date}), region, account)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // Fetch RI hours used
+	// data, err := sendQuery(svcAthena, conf.Athena.DbName, substituteParams(conf.RI.Sql, map[string]string{"**DATE**": date}), region, account)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	// loop through response data and generate map of hourly usage, per AZ, per instance, per platform
-	hours := make(map[string]map[string]map[string]map[string]int)
-	for row := range data.Rows {
-		_, ok := hours[data.Rows[row]["date"]]
-		if !ok {
-			hours[data.Rows[row]["date"]] = make(map[string]map[string]map[string]int)
-		}
-		_, ok = hours[data.Rows[row]["date"]][data.Rows[row]["az"]]
-		if !ok {
-			hours[data.Rows[row]["date"]][data.Rows[row]["az"]] = make(map[string]map[string]int)
-		}
-		_, ok = hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]]
-		if !ok {
-			hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]] = make(map[string]int)
-		}
+	// // loop through response data and generate map of hourly usage, per AZ, per instance, per platform
+	// hours := make(map[string]map[string]map[string]map[string]int)
+	// for row := range data.Rows {
+	// 	_, ok := hours[data.Rows[row]["date"]]
+	// 	if !ok {
+	// 		hours[data.Rows[row]["date"]] = make(map[string]map[string]map[string]int)
+	// 	}
+	// 	_, ok = hours[data.Rows[row]["date"]][data.Rows[row]["az"]]
+	// 	if !ok {
+	// 		hours[data.Rows[row]["date"]][data.Rows[row]["az"]] = make(map[string]map[string]int)
+	// 	}
+	// 	_, ok = hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]]
+	// 	if !ok {
+	// 		hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]] = make(map[string]int)
+	// 	}
 
-		v, _ := strconv.ParseInt(data.Rows[row]["hours"], 10, 64)
-		hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]][data.Rows[row]["platform"]] += int(v)
-	}
+	// 	v, _ := strconv.ParseInt(data.Rows[row]["hours"], 10, 64)
+	// 	hours[data.Rows[row]["date"]][data.Rows[row]["az"]][data.Rows[row]["instance"]][data.Rows[row]["platform"]] += int(v)
+	// }
 
-	// Create new cloudwatch client.
-	svcCloudwatch := cloudwatch.New(sess)
+	// // Create new cloudwatch client.
+	// svcCloudwatch := cloudwatch.New(sess)
 
-	// Iterate through each hour and compare the number of instances used vs the number of RIs available
-	// If RI leftover percentage is > 1% push to cloudwatch
-	for hour := range hours {
-		if err := riUtilizationHour(svcCloudwatch, hour, hours[hour], az_ri, region_ri, conf, region); err != nil {
-			return err
-		}
-	}
+	// // Iterate through each hour and compare the number of instances used vs the number of RIs available
+	// // If RI leftover percentage is > 1% push to cloudwatch
+	// for hour := range hours {
+	// 	if err := riUtilizationHour(svcCloudwatch, hour, hours[hour], az_ri, region_ri, conf, region); err != nil {
+	// 		return err
+	// 	}
+	// }
 	return nil
 }
 
